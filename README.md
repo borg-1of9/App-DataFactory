d190db2fae42cbea83e7bd4d70c7529a13f2b545
Vladislav Kantor <kantor.vladislav@gmail.com>
2026-05-31 10:36:23 +0200

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+App::DataFactory - It's new $module
+
+# SYNOPSIS
+
+    use App::DataFactory;
+
+# DESCRIPTION
+
+App::DataFactory is ...
+
+# LICENSE
+
+Copyright (C) Vladislav Kantor.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Vladislav Kantor <kantor.vladislav@gmail.com>
---
+++ lib/App/DataFactory.pm
@@ -0,0 +1,39 @@
+package App::DataFactory;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::DataFactory - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::DataFactory;
+
+=head1 DESCRIPTION
+
+App::DataFactory is ...
+
+=head1 LICENSE
+
+Copyright (C) Vladislav Kantor.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Vladislav Kantor E<lt>kantor.vladislav@gmail.comE<gt>
+
+=cut
+
